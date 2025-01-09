28fcb621d976ea5945973499a45f3bd9a4a2b7e5
Koinunopochi <koinunopoti@gmail.com>
2025-01-09 17:39:44 +0900

    feat: 問題作成時のzip保存に暗号化機能を追加

--- src/components/exam-editor/utils.ts
+++ src/components/exam-editor/utils.ts
@@ -1,4 +1,5 @@
 import JSZip from 'jszip';
+import { generateKeyPair, generateAESKey, encryptAESKey, encryptData } from '../../utils/crypto';
 
 // ユニークなIDを生成する関数
 export const generateUniqueId = (prefix: string = '') => {
@@ -10,11 +11,38 @@ export const generateUniqueId = (prefix: string = '') => {
 export const downloadZIP = async (files: Array<{name: string, data: any}>) => {
   const zip = new JSZip();
   const date = new Date().toISOString().slice(0, 10).replace(/-/g, '');
-  
-  // 各ファイルをzipに追加
-  files.forEach(file => {
-    zip.file(file.name, JSON.stringify(file.data, null, 2));
+
+  // 鍵ペアの生成
+  const keyPair = await generateKeyPair();
+  const aesKey = await generateAESKey();
+
+  // AES鍵を公開鍵で暗号化
+  const encryptedAesKey = await encryptAESKey(keyPair.publicKey, aesKey);
+
+  // 各ファイルを暗号化してzipに追加
+  const encryptedFiles = await Promise.all(files.map(async file => {
+    const encrypted = await encryptData(file.data, aesKey);
+    return {
+      name: file.name,
+      data: encrypted
+    };
+  }));
+
+  // 暗号化されたファイルをzipに追加
+  encryptedFiles.forEach(file => {
+    zip.file(`encrypted_${file.name}`, JSON.stringify(file.data, null, 2));
   });
+
+  // 鍵情報をzipに追加
+  const privateKeyJwk = await window.crypto.subtle.exportKey(
+    'jwk',
+    keyPair.privateKey
+  );
+
+  zip.file('key_info.json', JSON.stringify({
+    encryptedAesKey: Array.from(new Uint8Array(encryptedAesKey)),
+    privateKey: privateKeyJwk
+  }, null, 2));
 
   // zipファイルを生成してダウンロード
   const content = await zip.generateAsync({type: 'blob'});
